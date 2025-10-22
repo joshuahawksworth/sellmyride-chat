6e527b2a1708a0cc5cf3d8b60e4e4e35f97154d2
joshuahawksworth <joshuahawksworth@me.com>
2025-10-22 17:29:04 +0100

    PNG favicon in the browser tab

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SellMyRide Chat</title>
+    <link rel="icon" type="image/png" href="/favicon.png">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
